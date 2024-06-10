c54c88c5bbb702183d9498f2651cdcfde7680223
maple-rain <demonpanda1369@gmail.com>
2024-06-10 16:47:00 +0900

    1

--- Assets/_Project/Sripts/Character/Enemy.cs
+++ Assets/_Project/Sripts/Character/Enemy.cs
@@ -19,7 +19,7 @@ public class Enemy : MonoBehaviour
     private void Start()
     {
         player = FindObjectOfType<PlayerController>().transform;
-        transform.position = new Vector3(0.5f, 0f, -5f);
+        transform.position = new Vector3(0.5f, 0.2f, -5f);
         throwTimer = Random.Range(10f, 15f);
     }
 
@@ -27,15 +27,7 @@ public class Enemy : MonoBehaviour
     {
         if (player != null)
         {
-            FollowPlayer();
-
-            if (Mathf.Abs(transform.position.z - player.position.z) <= catchDistance)
-            {
-                Debug.Log("Game Over!");
-                //SceneManager.LoadScene("GameOverScene");
-            }
-
-            
+            FollowPlayer();           
             throwTimer -= Time.deltaTime;
             if (throwTimer <= 0f)
             {
@@ -52,22 +44,14 @@ public class Enemy : MonoBehaviour
     }
     private void ThrowWeapon()
     {
-        // 무기를 던지는 애니메이션 트리거
-        animator.SetTrigger("ThrowAnimation"); // 정확한 트리거 이름 사용
-
-        // 던지기 애니메이션 시간이 지난 후에 무기를 던짐
+        animator.SetTrigger("ThrowAnimation"); 
         StartCoroutine(ThrowWeaponAfterAnimation());
     }
 
     private IEnumerator ThrowWeaponAfterAnimation()
     {
-        // 애니메이션 길이만큼 대기
-        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
-
-        // 무기를 생성하고 던짐
+        yield return new WaitForSeconds(1.5f);
         GameObject weapon = Instantiate(weaponPrefab, throwPoint.position, Quaternion.identity);
-
-        // Rigidbody가 있는지 확인하고, 있으면 속도 설정
         Rigidbody weaponRb = weapon.GetComponent<Rigidbody>();
         if (weaponRb != null)
         {
@@ -78,10 +62,8 @@ public class Enemy : MonoBehaviour
             Debug.LogError("Weapon prefab does not have a Rigidbody component!");
         }
 
-        // 5초 후에 무기 제거
         Destroy(weapon, 5f);
 
-        // 달리는 상태로 돌아감
         animator.SetFloat("Moving", speed);
     }
 
